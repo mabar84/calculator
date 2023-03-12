dc6a09341589691cb47774b2a293ffb46d94f8ca
HUAWEI <mabar@tut.by>
2023-03-12 17:39:57 +0300

    add store and counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Keyboard} from './keyboard/keyboard';
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "./bll/store";
+import {incValueAC, setValueFromLocalStorageAC} from "./bll/counter-reducer";
 
 function App() {
 
@@ -9,8 +12,8 @@ function App() {
     const arr = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*', 0, 'C', '=', '/']
     const numbers = '0123456789'
     const signs = '+-*/'
-    let [value, setValue] = useState<string>('')
-    let [sign, setSign] = useState<string>('')
+    // let [value, setValue] = useState<string>('')
+    // let [sign, setSign] = useState<string>('')
     let [a, setA] = useState<string>('')
     let [b, setB] = useState<string>('')
 
@@ -20,18 +23,18 @@ function App() {
     const click = (symbol: string) => {
 
         if ('+-*/'.includes(symbol)) {
-            sign = symbol
-            setSign(sign)
+            // sign = symbol
+            // setSign(sign)
         }
 
-        setValue(value + symbol)
+        // setValue(value + symbol)
 
-        sign ? setB(b + symbol) : setA(a + symbol)
+        // sign ? setB(b + symbol) : setA(a + symbol)
 
         if (symbol === 'C') {
-            setA('')
-            setB('')
-            setValue('')
+            // setA('')
+            // setB('')
+            // setValue('')
         }
 
         if (symbol === '=') {
@@ -40,23 +43,34 @@ function App() {
             let n1=+a
             let n2=+b.slice(1,b.length)
 
-            console.log(sign)
+            // console.log(sign)
 
-            switch (sign) {
-                case '+':
-                    return setValue((n1 + n2).toString())
-                case '-':
-                    return setValue((n1 - n2).toString())
-                case '*':
-                    return setValue((n1 * n2).toString())
-                case '/':
-                    return setValue((n1 / n2).toString())
-            }
+            // switch (sign) {
+            //     case '+':
+            //         return setValue((n1 + n2).toString())
+            //     case '-':
+            //         return setValue((n1 - n2).toString())
+            //     case '*':
+            //         return setValue((n1 * n2).toString())
+            //     case '/':
+            //         return setValue((n1 / n2).toString())
+            // }
         }
     }
+    console.log('APP')
+    const value = useSelector<AppStateType, number>(state => state.counter.value)
+    const dispatch = useDispatch()
 
+    dispatch(setValueFromLocalStorageAC(value))
+
+    const incHandler = () => {
+        dispatch(incValueAC())
+    }
     return (
         <div className="App">
+
+            <h2>{value}</h2>
+            <button onClick={incHandler}>inc</button>
             <div className="body">
                 <span className={'input'}>{value}</span>
 
--- src/keyboard/keyboard.jsx
+++ src/keyboard/keyboard.jsx
@@ -1,14 +1,12 @@
+import {useEffect} from "react";
 
 export const Keyboard = (props) => {
-
-    const onClick=(e)=>{
+    const click=(e)=>{
         props.click(e.currentTarget.textContent)
     }
-
-    const buttons=props.arr.map((a)=>
-
-            //<button disabled={!'123456789'.includes(a)} onClick={onClick} className={'button'} key={a} id={a}>{a}</button>)
-            <button onClick={onClick} className={'button'} key={a} id={a}>{a}</button>)
+    const buttons=props.arr.map((el)=>
+            //<button disabled={!'123456789'.includes(el)} onClick={onClick} className={'button'} key={el} id={el}>{el}</button>)
+            <button onClick={click} className={'button'} key={el} id={el}>{el}</button>)
 
     return <div className={'keyboard'} >
         {buttons}
